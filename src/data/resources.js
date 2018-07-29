6f136c6f48ddc0225d4cd642b94f94cb86d43987
Zuri Hunter <zurihunter92@gmail.com>
2018-07-29 13:55:00 -0400

    Added some resources and rearrange stuff

--- src/data/resources.js
+++ src/data/resources.js
@@ -1,7 +1,8 @@
 var resources = [
   {
-    "title": "",
-    "url": "",
+    "title": "Academic Earth",
+    "url": "https://academicearth.org",
+    "age": "14-17",
     "description": "",
     "image": "",
     "categories": ["Web Development"]
@@ -9,58 +10,107 @@ var resources = [
   {
     "title": "Alexa Kids Skill Challenge",
     "url": "https://alexakidskills.devpost.com/",
+    "age": "10-13",
     "description": "",
     "image": "",
     "categories": ["Web Development", "Internet of Things"]
   },
   {
-    "title": "",
-    "url": "",
+    "title": "Open Culture",
+    "url": "https://www.openculture.com",
+    "age": "14-17",
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
   {
-    "title": "",
-    "url": "",
+    "title": "Olico",
+    "url": "https://learn.olico.org/",
+    "age": ["10-13", "14-17"],
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
   {
-    "title": "",
-    "url": "",
+    "title": "Alison",
+    "url": "https://alison.com/",
+    "age": "14-17",
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
   {
-    "title": "",
-    "url": "",
+    "title": "iTunesU",
+    "url": "https://www.apple.com/education/itunes-u/",
+    "age": "14-17",
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
   {
-    "title": "",
-    "url": "",
+    "title": "Stanford University Online Courses",
+    "url": "https://online.stanford.edu/courses",
+    "age": "14-17",
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
   {
-    "title": "",
-    "url": "",
+    "title": "Harvard University Online Courses",
+    "url": "https://online-learning.harvard.edu/",
+    "description": "",
+    "age": "14-17",
+    "image": "",
+    "categories": ["Web Development"]
+  },
+  {
+    "title": "Open Yale University Courses",
+    "url": "https://oyc.yale.edu/",
+    "age": "14-17",
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
   {
-    "title": "",
+    "title": "Class Central",
     "url": "",
+    "age": "14-17",
+    "description": "https://www.class-central.com/",
+    "image": "",
+    "categories": ["Web Development"]
+  },
+  {
+    "title": "Code.org",
+    "url": "https://code.org/learn",
+    "age": ["7-9", "10-13", "14-17"],
     "description": "",
     "image": "",
     "categories": ["Web Development"]
   },
+  {
+    "title": "Memrise",
+    "url": "https://www.memrise.com/courses/english/computers-engineering/",
+    "age": "14-17",
+    "description": "",
+    "image": "",
+    "categories": ["Web Development"]
+  },
+
+  {
+    "title": "CtrlPaint",
+    "url": "https://www.ctrlpaint.com/library/",
+    "age": "14-17",
+    "description": "",
+    "image": "",
+    "categories": ["Web Development"]
+  },
+  {
+    "title": "Kadenze",
+    "url": "https://www.kadenze.com/courses",
+    "age": "14-17",
+    "description": "",
+    "images": "",
+    "categories": ["Web Development"]
+  }
 
 ]
